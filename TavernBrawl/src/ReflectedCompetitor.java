3fd45e67b12b0d72ebf41cda204f86e0f8b6b691
Matin Ansaripour <matinansaripour@gmail.com>
2020-08-16 01:53:21 +0430

    reflection debug

--- TavernBrawl/src/ReflectedCompetitor.java
+++ TavernBrawl/src/ReflectedCompetitor.java
@@ -2,6 +2,11 @@ import Exceptions.EmptyDeckException;
 import Logic.Competitor;
 
 public class ReflectedCompetitor extends Competitor {
+
+    public ReflectedCompetitor(String username) {
+        super(username);
+    }
+
     @Override
     public void drawCard() throws EmptyDeckException {
         super.drawCard();
--- TavernBrawl/src/ReflectedGame.java
+++ TavernBrawl/src/ReflectedGame.java
@@ -1,7 +1,12 @@
+import Logic.Competitor;
 import Logic.Game;
 import Models.Cards.Minion;
 
 public class ReflectedGame extends Game {
+
+    public ReflectedGame(Competitor competitor1, Competitor competitor2, boolean isWithBot) {
+        super(competitor1, competitor2, isWithBot);
+    }
 
     @Override
     public void refreshMana() {
